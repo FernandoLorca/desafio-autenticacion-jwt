1b4cb11da4dcf0d87612ee8216abdf87012cf2db
FernandoLorca <florcapinto@gmail.com>
2023-06-09 23:05:00 -0400

    server running in port

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+import express, { json } from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
